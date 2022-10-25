7c2c63a577fed63a8190d36e152bfe61c5d59a6f
Gabegar <Gagarcia190@gmail.com>
2022-10-25 16:28:56 -0400

    created clear and reset functionality via clr btn
    Upon pressing, it will clear the current app's grid and prompt user.
    User can enter anything between 16 and 100. Recursion takes care of
    repetition, in the event the user wishes to continue using app.

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
-const containerGrid = document.querySelector(".container-grid");
-const body = document.querySelector("body");
-const sizeButton = document.createElement("button");
+function SketchApp() {
+    const containerGrid = document.querySelector(".container-grid");
+    const displayGridSize = document.querySelector(".display-grid-size");
+    const resetGridBtn = document.querySelector(".reset-grid-btn");
 
-function colorSquare(hover) {
-    if (hover.target.classList.value === "grid-item") {
-        hover.target.style.backgroundColor = "gray";
+    function colorSquare(hover) {
+        if (hover.target.classList.value === "grid-item") {
+            hover.target.style.backgroundColor = "gray";
+        }
     }
+
+    function clearGrid(event) {
+        containerGrid.textContent = "";
+        let newSize = prompt(
+            "How big would you like your grid?\n(e.g., typing in any number such as 10 => 10x10 grid)\nNote: Cannot be greater than 100 or less than 16."
+        );
+
+        if (newSize > 100 || newSize < 16) return clearGrid();
+        createGrid(newSize);
+        eventListeners();
+    }
+
+    function eventListeners() {
+        const allSquares = document.querySelectorAll(".container-grid div");
+        allSquares.forEach((square) => {
+            square.addEventListener("mouseover", colorSquare);
+        });
+    }
+
+    const createGrid = (size) => {
+        displayGridSize.textContent = `Grid Size: ${size} x ${size}`;
+        displayGridSize.style.fontSize = "24px";
+
+        containerGrid.setAttribute(
+            "style",
+            `display: grid; grid-template-columns: repeat(${size}, auto); 
+        grid-template-rows: repeat(${size}, auto);`
+        );
+
+        for (let i = 1; i <= size * size; i++) {
+            const newSquare = document.createElement("div");
+            newSquare.classList.add("grid-item");
+            containerGrid.appendChild(newSquare);
+        }
+    };
+
+    createGrid(16);
+
+    resetGridBtn.addEventListener("click", clearGrid);
+    eventListeners();
 }
 
-const createGrid = (size) => {
-    rows = size;
-    cols = size;
-
-    containerGrid.setAttribute(
-        "style",
-        `display: grid; grid-template-columns: repeat(${cols}, auto); 
-        grid-template-rows: repeat(${rows}, auto);`
-    );
-
-    for (let i = 1; i <= rows * cols; i++) {
-        const newSquare = document.createElement("div");
-        newSquare.classList.add("grid-item");
-        containerGrid.appendChild(newSquare);
-    }
-};
-
-createGrid(30);
-
-const allSquares = document.querySelectorAll(".container-grid div");
-
-allSquares.forEach((square) => {
-    square.addEventListener("mouseover", colorSquare);
-});
+SketchApp();
